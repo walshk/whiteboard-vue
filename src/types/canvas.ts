c609833d1447339beee520123bf8f916eb1bfc5c
walshk <kevin.walsh@rochester.edu>
2023-07-01 22:07:04 -0400

    added more methods for drawing and canvas classes

--- src/types/canvas.ts
+++ src/types/canvas.ts
@@ -8,4 +8,41 @@ export class Canvas {
 
 	ctx: CanvasRenderingContext2D;
 	drawings: Drawing[];
+
+	clear(): void {
+		this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+	}
+
+	redraw(): void {
+		for (let i = 0; i < this.drawings.length; i++) {
+			this.drawings[i].draw(this.ctx);
+		}
+	}
+
+	eraseAt(x: number, y: number): void {
+		const nearestDrawing: Drawing = this.findNearestDrawingTo(x, y);
+		const indexToRemove: number = this.drawings.findIndex(
+			(drawing: Drawing) => {
+				return drawing.id === nearestDrawing.id;
+			}
+		);
+
+		if (indexToRemove !== -1) {
+			this.drawings.splice(indexToRemove, 1);
+			this.clear();
+			this.redraw();
+		}
+	}
+
+	findNearestDrawingTo(x: number, y: number): Drawing {
+		const closestDrawing = this.drawings.reduce(
+			(prev: Drawing, curr: Drawing): Drawing => {
+				const distanceToPrev = prev.calculateDistanceFrom(x, y);
+				const distanceToCurr = curr.calculateDistanceFrom(x, y);
+
+				return distanceToPrev < distanceToCurr ? prev : curr;
+			}
+		);
+		return closestDrawing;
+	}
 }
--- src/types/drawing.ts
+++ src/types/drawing.ts
@@ -1,15 +1,49 @@
 import { Point } from './point';
 
 export class Drawing {
-	constructor() {
+	constructor(id: number, radius: number, color?: string) {
+		this.id = id;
+
 		this.points = [] as Point[];
+		this.radius = radius;
+		this.color = color;
 	}
 
+	id: number;
 	points: Point[];
+	radius: number;
+	color?: string;
 
-	draw(ctx: CanvasRenderingContext2D, radius: number, color?: string): void {
+	draw(ctx: CanvasRenderingContext2D): void {
 		for (let i = 0; i < this.points.length; i++) {
-			this.points[i].render(ctx, radius, color);
+			this.points[i].render(ctx, this.radius, this.color);
 		}
+	}
+
+	calculateDistanceFrom(x: number, y: number): number {
+		const distance: Function = (
+			x1: number,
+			x2: number,
+			y1: number,
+			y2: number
+		): number => {
+			// euclidean distance
+			const a: number = x1 - x2;
+			const b: number = y1 - y2;
+
+			return Math.sqrt(a * a + b * b);
+		};
+
+		// TODO: implement more efficient search method
+		let shortestDistance: number = 0;
+		for (let i = 0; i < this.points.length; i++) {
+			const point: Point = this.points[i];
+			const distanceToPoint: number = distance(x, point.x, y, point.y);
+
+			if (distanceToPoint < shortestDistance) {
+				shortestDistance = distanceToPoint;
+			}
+		}
+		return shortestDistance;
 	}
 }
